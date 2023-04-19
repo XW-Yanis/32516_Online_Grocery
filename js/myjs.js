9aa5d4b62df654231456a20ed07746327b735469
Xiang WENG <xwen3544@uni.sydney.edu.au>
2023-04-20 00:26:20 +1000

    Do more CSS on Navbar, and add a product card.

---
+++ js/myjs.js
@@ -0,0 +1,20 @@
+
+// $(document).ready(function () {
+//   $("#frozen").hover(function () {
+//     $("#frozen-ul").toggle();
+//   });
+
+//   $("#fresh").hover(function () {
+//     $("#fresh-ul").toggle();
+//   });
+//   $("#beverage").hover(function () {
+//     $("#beverage-ul").toggle();
+//   });
+//   $("#home").hover(function () {
+//     $("#home-ul").toggle();
+//   });
+//   $("#pet").hover(function () {
+//     $("#pet-ul").toggle();
+//   });
+
+});
